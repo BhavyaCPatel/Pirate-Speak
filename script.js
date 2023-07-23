86dcc1aa7f8ba7d7e03194c8b89bc65368072c09
BhavyaCPatel <bhavyaaes@gmail.com>
2023-07-23 19:14:44 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var input=document.querySelector("#textinput");
+var btn=document.querySelector("#btn-translate");
+var output=document.querySelector("#textoutput");
+
+btn.addEventListener("click",()=>{
+    console.log(input.value)
+    var inputText=input.value
+    fetch("https://api.funtranslations.com/translate/pirate.json?text="+inputText)
+    .then(response=>response.json())
+    .then(response=>{
+        console.log(response)
+        output.innerHTML=response.contents.translated
+    })
+    .catch(err=>{
+        alert(err)
+        console.log(err)})
+});
